141f4b577c9b114a45aef4f2676c50ee36a1f4d7
PRIST LEGACY <yiigavicent@gmail.com>
2023-04-02 12:41:20 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,53 @@
+
+bot_token = "5906860486:AAF9_DU9F_6Xk9tQq7rvls26HgJMzHyJJpY"
+
+
+class m:
+    admin = 'https://t.mepristlegacy'
+    force_channel = "pristbank"
+    startmsg = """
+I can help you in many things regarding to fixed games. Am smart but you can still contact the **UFM administration** for farther help.
+**So now can I know your need ?**
+Use below buttons for simplicity!
+    """
+    not_sub_msg = """Please subscribe to our main channel 📢 to use this BOT.
+After use /reload to proceed"""
+
+    helpmsg = 'THIS A HELP MESSAGE DESCRIBING ALL WAYS AND HOW TO GET ONE RICH IN BETTING. ALL COMMANDS AND SHORT CODES ARE FOUND HERE'
+    commands = {  # command description used in the "help" command
+    'start'       : 'Start the bot again',
+    'help'        : 'Gives you information about the available services and how to use the bot',
+    'free': 'To quickly access FREE TIPS.',
+    'getImage'    : 'A test using multi-stage messages, custom keyboard, and media sending',
+    'commands'    : 'All commands available with the bot',
+    'admin'    : 'Get admin link. You can also type admin for quick access.'
+}
+
+    free_msg = """‼️Caution:\n
+Here at our platform, we know things are hard so we came up with the FREE TIPS department where the administrator provides you with free predictions. \nWhat does this mean, matches provided here are not 100% sure. They most times win but stake them on your own risk, losses are not on us.
+\nFor 100% sure matches, join our VIP GAMES or else you may proceed to see today's free matches👇"""
+
+
+    vip_msg = "VIP MSG"
+    vipmenu_msg = """VIP MATCHES ARE 100% SURE GAMES.
+WINNING IS GUARANTEED. IN SHORT, THEY ARE RISK FREE MATCHES.
+STAKE HIGH ON THEM."""
+    cs_msg = """YOU ARE ABOUT TO BUY
+Match Type: Correct Score
+Count: 2
+ODDS: 200+
+
+Confirm And Proceed To Booking"""
+    country_msg = "SPECIFY YOUR COUNTY"
+    admin_msg = """
+🧑‍💻ADMIN CONTACT\n
+
+You be redirected to the admin's box.\n Please try to be brief and precise\n
+
+IE: Be on point because there are many people in that box.
+"""
+
+    vipdes_msg = "VIP DESCRIPTION"
+    how_msg = "THIS DESCRIBES HOW TO BOOK MATCHES BOTH VIP AND VVIP"
+
+
