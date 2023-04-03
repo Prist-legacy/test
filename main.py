095e6c987cd4cc002964b67134b75fa1cbdd60ff
PRIST LEGACY <yiigavicent@gmail.com>
2023-04-03 22:44:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -502,9 +502,10 @@ def process_menu_step(message):
     user = user_dict[message.chat.id]
     try:
         if message.text == "Yes":
-            bot.send_message(message.chat.id, text=f'*Fowarded successfully...*',
-                               parse_mode = "Markdown",
-                               reply_markup=main_btn())
+            bot.forward_message(
+            chat_id="@pristlegacy", 
+            from_chat_id=message.chat.id,message_id=message.message_id
+            )
         elif message.text == "No":
              msg = bot.send_message(message.chat.id, text='*Retype your problem*',
                          parse_mode = "Markdown",
