234c2ca6300e3d0777e3071ba0a898a3f4c6b4c4
PRIST LEGACY <yiigavicent@gmail.com>
2023-04-07 09:02:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ Here at our platform, we know things are hard so we came up with the FREE TIPS d
 
     vip_msg = """*CORRECT SCORE:* _This involves providing half-time and full-time score of the dedicated matches. Matches can be daily or else source hasn't confirmed it. They are 100% sure; we provide match score before it kicks off._\n*HT/FT:* _These are matches give results by halves,▫️*Halftime results to Full-time results (1/2)*👉Where home team wins half-time then away team to win full-time._
     
-    All results are delivered earlier before they kickoff."""
+    `All results are delivered earlier before they kickoff.`"""
     vipmenu_msg = """VIP MATCHES ARE 100% SURE GAMES.
 WINNING IS GUARANTEED. IN SHORT, THEY ARE RISK FREE MATCHES.
 STAKE HIGH ON THEM."""
