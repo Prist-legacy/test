cb7e94e504bdb72304c15f85b06f7f19b65b9b33
PRIST LEGACY <yiigavicent@gmail.com>
2023-04-08 10:17:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -679,7 +679,7 @@ def send_cast(message):
     
     #fetch users
     if user not in m.admin:
-        bot.send_message(message.chat.id,text="You require admin permission to do this ‼️,parse_mode = "Markdown")
+        bot.send_message(message.chat.id,text="You require admin permission to do this ‼️",parse_mode = "Markdown")
     else;
         bot.send_message(users,text=msg)
     
