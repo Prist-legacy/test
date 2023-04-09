5f74fa7b6a6c0aeb7aa72e37d9d9b82377ecfa09
PRIST LEGACY <yiigavicent@gmail.com>
2023-04-09 18:14:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -604,7 +604,7 @@ def send_welcome(message):
 
     cursor = conn.cursor()
 
-    postgreSQL_select_Query = f"select free_tips from free_tips where tips_date='{}'"
+    postgreSQL_select_Query = "select free_tips from free_tips where tips_date='{}'"
 
     cursor.execute(postgreSQL_select_Query.format(current_date))
 
