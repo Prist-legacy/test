014173241eeebf71708b373bdf181cba8fbf836b
PRIST LEGACY <yiigavicent@gmail.com>
2023-04-17 15:33:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -950,7 +950,7 @@ def get_message(message):
             bot.send_message(message.chat.id, text=m.main_msg,reply_markup=main_btn()
                          ,parse_mode = "Markdown")
         elif message.text == "Admin":
-            msg = "*CONTACT ADMIN HERE*\n\n⚠️Contact admin here for payment issues only. Click on UFM ADMIN below 👇.\n*🧑‍💻ADMIN:* [UFM ADMIN]({})\n\n_All rights reserved._")
+            msg = "*CONTACT ADMIN HERE*\n\n⚠️Contact admin here for payment issues only. Click on UFM ADMIN below 👇.\n*🧑‍💻ADMIN:* [UFM ADMIN]({})\n\n_All rights reserved._"
             bot.send_message(message.chat.id, text=msg.format(m.admin_direct),reply_markup=main_btn(),disable_web_page_preview=True
                          ,parse_mode = "Markdown")
         
