56681fc106aea47747937e155363d1c9f8e5df7d
PRIST LEGACY <yiigavicent@gmail.com>
2023-04-26 17:22:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -866,9 +866,9 @@ def send_cast(message):
            bot.send_message(message.chat.id,text=f"You require admin permission to do this ‼️",parse_mode = "Markdown")
         else:
             for user in users:
-            #bot.send_message(message.chat.id,f'_These are your users_\n{users}',parse_mode = "Markdown")
-                    bot.send_message(chat_id=jip,text=msg,parse_mode = "Markdown")
                 
+            #bot.send_message(message.chat.id,f'_These are your users_\n{users}',parse_mode = "Markdown")
+                bot.send_message(chat_id=jip,text=msg,parse_mode = "Markdown")
     except Exception as e:
         #*\n`These are your users`\n{users}
         bot.send_message(message.chat.id, 
