d2d7fbb17bde7207d8cc85fc54d0d3ee211bf7b3
PRIST LEGACY <yiigavicent@gmail.com>
2023-04-26 13:12:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1030,6 +1030,7 @@ def get_message(message):
             except Exception as e:
                 bot.send_message(message.chat.id, "⚠️_CORRECT SCORE matches haven't been received from the source. Please wait, you will be informed in the main channel when they arrive. Thanks_"
                                  ,reply_markup=main_btn(),parse_mode = "Markdown")
+                bot.send_message(m.admin,text=e,parse_mode = "Markdown")
         elif message.text == "HT/FT":
             msg = "*🔘HT/FT MENU*\n\n_⚠️No HT/FT matches today.\nPlease proceed with CORRECT SCORE matches._"
             bot.send_message(chat_id=message.chat.id,
